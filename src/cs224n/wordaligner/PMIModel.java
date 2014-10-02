406e3e8c06517ac8641fb1531625b652a7b63c9f
John Doherty <john.doherty13@gmail.com>
2014-10-01 22:40:44 -0700

    added PMIModel

---
+++ src/cs224n/wordaligner/PMIModel.java
@@ -0,0 +1,19 @@
+package cs224n.wordaligner;
+
+import java.util.List;
+
+public class PMIModel implements WordAligner {
+
+	@Override
+	public Alignment align(SentencePair sentencePair) {
+		Alignment alignment = new Alignment();
+		return alignment;
+	}
+
+	@Override
+	public void train(List<SentencePair> trainingData) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
